5f0135961dd71e9b2e6a43b856e38d0fd1d3faba
atsuo1203 <dondonkitemasu@gmail.com>
2018-08-08 13:18:33 +0900

    flip_item

---
+++ app/models/flip_item.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, ForeignKey
+
+from app.models import Base, row_to_dict, session_scope
+from app.models.flip import Flip
+from app.models.item import Item
+
+
+class FlipItem(Base):
+    __tablename__ = 'flip_item'
+
+    flip_item_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    flip_id = Column(
+        Integer,
+        ForeignKey(Flip.flip_id),
+        nullable=False
+    )
+    item_id = Column(
+        Integer,
+        ForeignKey(Item.item_id),
+        nullable=False
+    )
+
+    @classmethod
+    def all(cls):
+        with session_scope() as session:
+            rows = session.query(cls).all()
+
+            return [row_to_dict(row) for row in rows]
+
