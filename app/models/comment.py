140050a88308ece013382d446506d35a4122ce7a
atsuo1203 <dondonkitemasu@gmail.com>
2018-08-08 13:40:09 +0900

    comment

--- app/models/comment.py
+++ app/models/comment.py
@@ -1,12 +1,8 @@
-from datetime import datetime, timedelta, timezone
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
 
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
-from app.models import Base, session_scope, row_to_dict
-from app.models.thread import Thread
+from app.models import Base, row_to_dict, session_scope
+from app.models.flip import Flip
 from app.models.user import User
-
-
-JST = timezone(timedelta(hours=+9), 'JST')
 
 
 class Comment(Base):
@@ -18,70 +14,24 @@ class Comment(Base):
         nullable=False,
         autoincrement=True
     )
-    thread_id = Column(
-        Integer,
-        ForeignKey(Thread.thread_id),
-        nullable=False,
-        index=True
-    )
-    name = Column(String(length=256), nullable=False)
-    text = Column(String(length=256), nullable=False)
-    create_at = Column(
-        DateTime, nullable=False
-    )
     user_id = Column(
         Integer,
         ForeignKey(User.user_id),
         nullable=False,
+        index=True
     )
+    flip_id = Column(
+        String,
+        ForeignKey(Flip.flip_id),
+        nullable=False
+    )
+    create_at = Column(DateTime, nullable=False)
+    content = Column(String(length=512), nullable=False)
 
     @classmethod
-    def get_all(cls):
+    def all(cls):
         with session_scope() as session:
             rows = session.query(cls).all()
 
-            result = [row_to_dict(row) for row in rows]
+            return [row_to_dict(row) for row in rows]
 
-            return result
-
-    @classmethod
-    def get_all_by_t_id(cls, thread_id):
-        with session_scope() as session:
-            rows = session.query(cls).filter(
-                cls.thread_id == thread_id
-            ).all()
-
-            result = [row_to_dict(row) for row in rows]
-
-            return result
-
-    @classmethod
-    def post(cls, params):
-        # length_check
-        text = params.get('text')
-        if len(text) > 200:
-            raise Exception('over text length')
-
-        with session_scope() as session:
-            if not params.get('create_at'):
-                params.update({'create_at': datetime.now(JST)})
-
-            data = cls(**params)
-            session.add(data)
-            session.flush()
-
-            # thread_idに紐づくthreadのcomment_count加算
-            cls.update_thread(
-                session=session,
-                thread_id=params.get('thread_id')
-            )
-
-    @classmethod
-    def update_thread(cls, session, thread_id):
-        data = Thread(
-            thread_id=thread_id,
-            comment_count=(Thread.comment_count + 1),
-            update_at=datetime.now(JST)
-        )
-
-        session.merge(data)
