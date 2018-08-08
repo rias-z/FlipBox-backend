cd536a8536ea27f1cece9a210986d5b834d351d6
atsuo1203 <dondonkitemasu@gmail.com>
2018-08-08 13:55:52 +0900

    user_notify

---
+++ app/models/user_notify.py
@@ -0,0 +1,34 @@
+from sqlalchemy import Column, Integer, ForeignKey
+
+from app.models import Base, row_to_dict, session_scope
+from app.models.notify import Notify
+from app.models.user import User
+
+
+class UserNotify(Base):
+    __tablename__ = 'user_notify'
+
+    user_notify_id = Column(
+        Integer,
+        primary_key=True,
+        nullable=False,
+        autoincrement=True
+    )
+    user_id = Column(
+        Integer,
+        ForeignKey(User.user_id),
+        nullable=False
+    )
+    notify_id = Column(
+        Integer,
+        ForeignKey(Notify.notify_id),
+        nullable=False
+    )
+
+    @classmethod
+    def all(cls):
+        with session_scope() as session:
+            rows = session.query(cls).all()
+
+            return [row_to_dict(row) for row in rows]
+
