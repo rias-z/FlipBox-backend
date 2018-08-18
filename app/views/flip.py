7f3998d68918bf33be374295419bdcd21a41764d
atsuo1203 <dondonkitemasu@gmail.com>
2018-08-18 10:53:32 +0900

    flip編集

--- app/views/flip.py
+++ app/views/flip.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify, make_response, request
+from datetime import datetime, timezone, timedelta
 
 from app.models.flip import Flip
 from app.models.flip_item import FlipItem
@@ -9,6 +10,7 @@ from app.views.utils.check_webtoken import check_webtoken
 
 
 app = Blueprint('flip', __name__)
+JST = timezone(timedelta(hours=+9), 'JST')
 
 
 @app.route('/flip/<flip_id>', methods=['GET'])
@@ -37,22 +39,30 @@ def get_if_no_login(flip_id):
 @check_webtoken(extra_token=True)
 def post(token_data):
     try:
-        print(token_data)
-        flip = Flip.post(request.data.flip)
-        for i in request.data.items:
-            item = Item.post(i)
-            params = {'flip_id': flip.id, 'item_id': item.id}
-            FlipItem.post(params)
+        params = request.json
 
-        for i in request.data.tags:
-            tag = Tag.post(i)
-            params = {'flip_id': flip.id, 'tag_id': tag.id}
-            FlipTag.post(params)
+        user_id = token_data.get('user_id')
+        p_flip = params.get('flip')
+        p_tags = p_flip.pop('tags')
+        p_items = params.get('items')
+        items_cnt = len(p_items)
 
-        items = FlipItem.get_by_flip_id(flip.id)
-        tags = FlipTag.get_by_flip_id(flip.id)
+        for item in p_items:
+            item['item_id'] = str(datetime.now().timestamp()).replace('.', '')
+            Item.post(item)
+        for tag in p_tags:
+            p_tag = {'name': tag, 'tag_cnt': 1}
+            Tag.post(p_tag)
 
-        result = {'flip': flip, 'items': items, 'tags': tags}
+        p_flip['flip_id'] = str(datetime.now().timestamp()).replace('.', '')
+        p_flip['bookmark_cnt'] = 0
+        p_flip['user_id'] = user_id
+        p_flip['item_cnt'] = items_cnt
+        p_flip['good_cnt'] = 0
+        p_flip['create_at'] = datetime.now(JST)
+        flip = Flip.post(p_flip)
+
+        result = {'flip_id': flip.get('flip_id')}
         return jsonify(result)
     except Exception as e:
         return make_response('', 500)
